bb5984b6ad040c8afc700180149402b899107fd2
xuwenjie <1054999089@qq.com>
2020-04-29 23:43:36 +0800

    修改为路由传参的形式提交给controller

--- resources/views/tasks/_createTask.blade.php
+++ resources/views/tasks/_createTask.blade.php
@@ -1,6 +1,6 @@
 <div class="col-auto">
 
-{!! Form::open(['route'=>'tasks.store','method'=>'POST']) !!}
+{!! Form::open(['route'=>['tasks.store','project_id'=>$project->id],'method'=>'POST']) !!}
     <div class="input-group mb-2">
       <div class="input-group-prepend">
         <div class="input-group-text">
@@ -8,7 +8,8 @@
         </div>
       </div>
     {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>'请填写一个任务']) !!}
-    {!! Form::hidden('project_id', $project->id) !!}
+    
+    {{-- {!! Form::hidden('project_id', $project->id) !!} //使用hidden表单的方法--}}
     
     </div>
 
