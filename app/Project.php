3fd4142e0682c6e1de7d875426d2305bf6db980a
xuwenjie <1054999089@qq.com>
2020-04-29 22:52:57 +0800

    建立Project和Task一对多的关系

--- app/Project.php
+++ app/Project.php
@@ -12,5 +12,8 @@ class Project extends Model
     	//$project->user
     	return $this->belongsTo('App\User','user_id');
     	//默认会通过表明_id去寻找对应的字段，如果非一般命名，需要写明第二个foreign_key参数
-    }
+	}
+	public function tasks(){
+		return $this->hasMany('App\Task');
+	}
 }
