7572fc0aeac2399c10c0bbe03f9862c750b88a21
xuwenjie <1054999089@qq.com>
2020-05-12 01:28:52 +0800

    修改文件名，使projects表优先被创建，防止tasks表定义外键时，报错

---
+++ database/migrations/2020_04_09_011319_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Boolean;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('completion');
+            $table->integer('project_id')->unsigned();//全部设置为正整数
+            $table->timestamps();
+        
+            //$table->foreign('project_id')->references('id')->on('projects');//一个项目删除后，对应的任务也会删除
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
