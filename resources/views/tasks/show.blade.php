319619ee0b3fe6db80f29094c10f5ce96a6439fc
xuwenjie <1054999089@qq.com>
2020-06-02 00:50:00 +0800

    添加了编辑名称、显示数目、选择显示、全部处理的功能

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -8,13 +8,16 @@
         <div class="row justify-content-center">
             <div class="col-4 mr-4">
                 <div class="card mb-5">
-                    <div class="card-header">进行中</div>
+                    <div class="card-header">
+                        进行中 (@{{ inProcessed.length }})
+                        <button class="btn btn-success btn-sm float-right" v-on:click="allProcessed()">全部完成</button>
+                    </div>
                     <div class="card-body">
                         {{-- 在html中可以使用两个花括号，但是在laravel中已经被blade占用 --}}
                         <ul class="list-group">
                             {{-- 实现迭代数组里的对象，输出其属性 --}}
                             <li class="list-group-item" v-for="(step, index) in inProcessed">
-                                @{{ step.name }} 
+                                <span v-on:dblclick="edit(step)">@{{ step.name }}</span>
                                 <div class="float-right">
                                     <i class="fas fa-check" v-on:click="toggle(step)"></i>
                                     <i class="far fa-trash-alt pl-4" v-on:click="remove(step)"></i>
@@ -29,19 +32,23 @@
                         <div class="form-group">
                             <label>请输入待完成的步骤：</label>
                             {{-- 表单与数据进行响应式双向绑定，互相影响 --}}     
-                            <input type="text" class="form-control" v-model="newStep" v-on:keyup.enter="addStep">
+                            <input type="text" class="form-control" ref="addStep" v-model="newStep" v-on:keyup.enter="addStep">
+                            <button v-if="newStep" class="btn btn-success btn-sm float-right mt-3" v-on:click="addStep">添加步骤</button>
                         </div>
                     </div>
                 </div>
             </div>  {{-- .col-4 --}}
 
             <div class="col-4">
-                <div class="card">
-                    <div class="card-header">已完成</div>
+                <div class="card" v-show="Processed.length">
+                    <div class="card-header">
+                        已完成 (@{{ Processed.length }})
+                        <button class="btn btn-danger btn-sm float-right" v-on:click="allRemove()">全部清除</button>
+                    </div>
                     <div class="card-body">
                         <ul class="list-group">
                             <li class="list-group-item" v-for="(step, index) in Processed">
-                                @{{ step.name }}
+                                <span v-on:dblclick="edit(step)">@{{ step.name }}</span>
                                 <div class="float-right">
                                     <i class="fas fa-angle-left"  v-on:click="toggle(step)"></i>
                                     <i class="far fa-trash-alt pl-4" v-on:click="remove(step)"></i>
@@ -81,11 +88,25 @@
                     step.completion = ! step.completion
                 },
                 remove(step){
-                    let i = this.steps.indexOf(step)
-                    this.steps.splice(i, 1)
+                    let i = this.steps.indexOf(step)//获取下标
+                    this.steps.splice(i, 1)//移除
+                },
+                edit(step){
+                    this.remove(step)//移除
+                    this.newStep=step.name//放入输入框
+                    this.$refs.addStep.focus()//DOM元素聚焦
+                    // $('input').focus()
+                },
+                allProcessed(){
+                    this.inProcessed.forEach((step)=>{
+                        step.completion=true
+                    })
+                },
+                allRemove(){
+                    this.steps = this.inProcessed //让原始属性等于未完成计算属性，相当于清空了已完成
                 }
             },
-        //计算属性
+        //计算属性，本质是属性
         computed:{
             inProcessed(){
                 return this.steps.filter(function(step){
